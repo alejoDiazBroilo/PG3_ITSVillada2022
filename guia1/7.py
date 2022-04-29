7b31fcb9908433c3ea33bcc8dac7c0ca7665b215
Alejito <diazbroiloalejol@gmail.com>
2022-04-29 09:52:33 -0300

    parte 7 Completa

--- guia1/7.py
+++ guia1/7.py
@@ -1,11 +1,19 @@
 def ej7():
-    holder = str(input("numero: "))
-    for i in range(holder):
-        if not(holder[i] == holder[i] + 1 or holder[i] == holder[i] - 1):
-            return "no es step"
-    return "es step"
+    esStep = True
     try:
-        print("")
+        holder = str(input("numero: "))
+        if len(holder) % 2 != 0:
+            return "no es par, no se permite"
+        else:
+            for i in range(len(holder) - 1):
+                print(f"{holder[i]} - {holder[i + 1]} = {abs(int(holder[i]) - int(holder[i + 1]))}")
+                if abs(int(holder[i]) - int(holder[i + 1])) == 1:
+                    pass
+                else:
+                    esStep = False
+                    break
+        return f"es step? RTA: {esStep}"
     except:
         return "ERROR.."
 
+print(ej7())
