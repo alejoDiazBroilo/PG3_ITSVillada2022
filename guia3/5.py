3adb56bbfa83a3bd02d408351399767fcb90a389
alejo desde villada <diazbroiloalejol@gmail.com>
2022-05-05 08:49:33 -0300

    trabajo5 terminado

--- guia3/5.py
+++ guia3/5.py
@@ -1,23 +1,25 @@
+from copyreg import add_extension
+
+
 archivo = open("./guia3/trabajo5.txt", "w")
 def ej1():
     myBool = True
     while myBool:
         try:
             dato1 = str(input("_dame dato str para que no tire error: "))
-            dato2 = input("_dame dato int para que tire error: ")
-            archivo.write(dato1)
-            archivo.write(" / mensaje guardad\n")
-            archivo.write(dato2)
-            archivo.write(" / mensaje guardad\n")
+            dato2 = int(input("_dame dato int para que tire error: "))
 
-            if(int(input("_desea continuar? Any Key = si / 1 = no: " )) == 1):
-                myBool = False
-            else:
-                myBool = True
-        except ValueError as datoQueNoEsNumero:
-            print("*dato ingresado no valido" + str(datoQueNoEsNumero))
-        except TypeError as datoQueNoEsNumero:
+            archivo.write(dato1)
+            archivo.write(" / mensaje guardado\n")
+
+            archivo.write(dato2)
+            archivo.write(" / mensaje guardado\n")
+        except ValueError:
+            print("*se ingreso un str en dato2 que es un int: ")
+        except TypeError:
             print("*TEOOOOOOOOOOOOOOOOOOOOOOOOOOOOO no se pueden poner ints en un txt")
+            print(f"*igual el mensaje: {dato1}, se guardo en el txt")
+            archivo.close()
         
 ej1()
 archivo.close()
